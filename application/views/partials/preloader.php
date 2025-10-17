e47847256982a5fb5e7a5fceee27ff2bb1f5e0ec
ivanpakpahan2702 <ivanpakpahanchrst@gmail.com>
2025-10-17 07:24:58 +0700

    -

---
+++ application/views/partials/preloader.php
@@ -0,0 +1,4 @@
+<div class="preloader flex-column justify-content-center align-items-center">
+    <img class="animation__wobble" src="https://upload.wikimedia.org/wikipedia/commons/0/0f/Logo_Mahkamah_Agung_RI.png"
+        alt="MALogo" width="120">
+</div>
